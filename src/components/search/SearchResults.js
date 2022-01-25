49f822037aaed8c21ee93389e6f7a14f55c6feaf
sorachung <sorachung.src@gmail.com>
2022-01-25 15:53:16 -0600

    changed styling on search results again

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -12,17 +12,20 @@ export default () => {
                 <React.Fragment>
                     <h2>Matching Animals</h2>
                     <section className="animals">
-                        {location.state.animals.map(animal => {
-                            return (
-                                <Link className="result"
-                                to={{
-                                    pathname: `/animals/${animal.id}`
-                                }}>
-                                    {animal.name}
-                                </Link>
-                            )
-                    
-                        })}
+                        <ul className="searchResult__list">
+                            {location.state.animals.map(animal => {
+                                return (
+                                    <li>
+                                        <Link className="result"
+                                        to={{
+                                            pathname: `/animals/${animal.id}`
+                                        }}>
+                                            {animal.name}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </section>
                 </React.Fragment>
             )
@@ -34,18 +37,22 @@ export default () => {
             return (
                 <React.Fragment>
                     <h2>Matching Employees</h2>
-                    <section className="employees">
-                        {location.state.employees.map(employee =>  {
-                            return (
-                                <Link className="result"
-                                to={{
-                                    pathname: `/employees/${employee.id}`
-                                }}>
-                                    {employee.name}
-                                </Link>
-                            )
-                        
-                        })}
+                    <section className="employees searchResultChunk">
+                        <ul className="searchResult__list">
+                            {location.state.employees.map(employee =>  {
+                                return (
+                                    <li>
+                                        <Link className="result"
+                                        to={{
+                                            pathname: `/employees/${employee.id}`
+                                        }}>
+                                            {employee.name}
+                                        </Link>
+                                    </li>
+                                )
+                            
+                            })}
+                        </ul>
                     </section>
                 </React.Fragment>
             )
@@ -57,18 +64,22 @@ export default () => {
             return (
                 <React.Fragment>
                     <h2>Matching Locations</h2>
-                    <section className="locations">
-                        {location.state.locations.map(loc => {
-                            return (
-                                <Link className="result"
-                                to={{
-                                    pathname: `/locations/${loc.id}`
-                                }}>
-                                    {loc.name}
-                                </Link>
-                            )
-                            
-                        })}
+                    <section className="locations searchResultChunk">
+                        <ul className="searchResult__list">
+                            {location.state.locations.map(loc => {
+                                return (
+                                    <li>
+                                        <Link className="result"
+                                        to={{
+                                            pathname: `/locations/${loc.id}`
+                                        }}>
+                                            {loc.name}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+
+                        </ul>
                     </section>
                 </React.Fragment>
             )
